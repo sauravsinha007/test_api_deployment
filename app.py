0ddb2e940c0c892deec262938649bbed570ae1f2
sauravsinha007 <sauravsinha007@gmail.com>
2024-07-15 10:46:21 +0530

    implement flask
    install flask
    run app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello python")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return jsonify({'message': 'Hello, Python World!'})
+
+if __name__ == '__main__':
+  app.run()
